11ffa9141575ec293165b28943e00f44064c22ec
KhushiKumari27X <khushikumarikh27@gmail.com>
2026-04-25 22:50:23 +0530

    Updated API URL to deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,15 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+//   withCredentials: true
+// });
+
+// export default API/;
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://fitfusion-backend-f8j6.onrender.com/api",
   withCredentials: true
 });
 
