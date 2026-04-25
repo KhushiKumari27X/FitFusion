aec17aa5de8946287bbb11a8e757a5bc8da7a048
KhushiKumari27X <khushikumarikh27@gmail.com>
2026-04-26 00:48:33 +0530

    fix cors for vercel

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+// app.use(cors({
+//   origin: "http://localhost:3000",
+//   credentials: true
+// }));
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://fit-fusion-khmz.vercel.app"],
   credentials: true
 }));
 
